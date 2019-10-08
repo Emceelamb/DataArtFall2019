e233edbc64bf3f24528adb7c3c10863bb7da09cc
emceelamb <mlam@berkeley.edu>
2019-10-08 15:04:01 -0400

    counting random numbers

--- project-2/reader.py
+++ project-2/reader.py
@@ -1,3 +1,6 @@
+import sys
+import time
+
 filepath = 'digits.txt'
 
 ds = []
@@ -13,3 +16,54 @@ with open(filepath) as digits:
         count+=1
         
 print(len(ds))
+
+def adder():
+    num0 = 0
+    num1 = 0
+    num2 = 0
+    num3 = 0
+    num4 = 0
+    num5 = 0
+    num6 = 0
+    num7 = 0
+    num8 = 0
+    num9 = 0
+
+    for x in range(len(ds)):
+        d = int(ds[x])
+        if d == 0:
+            num0 += 1
+        if d == 1:
+            num1 += 1
+        if d == 2:
+            num2 += 1
+        if d == 3:
+            num3 += 1
+        if d == 4:
+            num4 += 1
+        if d == 5:
+            num5 += 1
+        if d == 6:
+            num6 += 1
+        if d == 7:
+            num7 += 1
+        if d == 8:
+            num8 += 1
+        if d == 9:
+            num9 += 1
+
+        print('\n')
+        print("0: ", num0)
+        print("1: ", num1)
+        print("2: ", num2)
+        print("3: ", num3)
+        print("4: ", num4)
+        print("5: ", num5)
+        print("6: ", num6)
+        print("7: ", num7)
+        print("8: ", num8)
+        print("9: ", num9)
+        print('Total count: ', x)
+        time.sleep(0.01)
+
+adder()
